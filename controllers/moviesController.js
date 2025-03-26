20bbf831c5e19ade3e3b68b24919df37c47fc5b6
NdikumNdifor <sebastienndifor@gmail.com>
2025-03-26 09:49:15 +0100

    Implemented post, update and delete route and round up swagger documentation.

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,14 +1,74 @@
 const moviesModel = require('../models/moviesModel')
 
 const listAllMovies = async (req, res) => {
+  //#swagger.tags = ['Movies']
+  /* #swagger.parameters['body'] = {
+      in: 'body',
+      description: 'List all movies',
+      required: true,
+      schema: { $ref: '#/definitions/Movie' }
+  } */
   try {
     const movies = await moviesModel.getAllMovies()
     res.setHeader('Content-Type', 'application/json')
     res.status(200).json(movies)
   } catch (err) {
-    console.error('Error fetching movies:', err.message)
-    res.status(500).json({ message: err.message || 'Internal server error' })
+    console.error('Error fetching movies', err.message)
+    res.status(500).json({ message: err.message || 'Internal server error.' })
   }
 }
 
-module.exports = { listAllMovies }
+const insertAMovie = async (req, res) => {
+  //#swagger.tags = ['Movies']
+  /* #swagger.parameters['body'] = {
+      in: 'body',
+      description: 'Add a movie to list',
+      required: true,
+      schema: { $ref: '#/definitions/Movie' }
+  } */
+  try{
+    const objectData = req.body
+    const movie = await moviesModel.addASingleMovie(objectData);
+    res.setHeader('Content-Type', 'application/json')
+    res.status(201).json({message:'You successfully created a new movie', id: movie._id})
+  }catch(err){
+    console.error('Failed to insert movie')
+    res.status(400).json({message: err.message || 'A server error occured.'})
+  }
+}
+
+const modifyAMovie = async (req, res) => {
+  try{
+    const movieId = req.params.id
+    const movieData = req.body
+    if(!movieData.title || !movieData.director|| !movieData.releaseYear || !movieData.genre || !movieData.rating || !movieData.duration || !movieData.cast){
+      throw new Error('All the fields must be included')
+    }
+    const updatedMovie = await moviesModel.updateAMovie(movieId, movieData)
+    res.setHeader('Content-Type', 'application/json')
+    res.status(201).json({message:'Movie was updated succesfully', id: updatedMovie._id})
+  }catch(err) {
+    res.status(500).json({message: err.message || 'A server error occured.'})
+  }
+}
+
+const removeAMovie = async (req, res) =>{
+  const deletedMovie = await moviesModel.deleteAMovie(req.params.id);
+  res.setHeader('Content-Type', 'application/json');
+  
+  if (deletedMovie.error) {
+    return res.status(400).json(deletedMovie); // Send the error message from the model function
+  }
+
+  res.status(200).json(deletedMovie);
+  // try{
+  //   const movieId = req.params.id
+  //   const deletedMovie = moviesModel.deleteAMovie(movieId)
+  //   res.setHeader('Content-Type', 'application/json')
+  //   res.status(200).json(deletedMovie)
+  // }catch(err){
+  //   res.status(500).json({error: err.message})
+  // }
+}
+
+module.exports = { listAllMovies, insertAMovie, modifyAMovie, removeAMovie }
--- models/moviesModel.js
+++ models/moviesModel.js
@@ -1,4 +1,5 @@
 const MovieModel = require('./movieSchema-model')
+const { ObjectId } = require('mongodb');
 
 const getAllMovies = async () => {
   try {
@@ -12,4 +13,66 @@ const getAllMovies = async () => {
   }
 }
 
-module.exports = { MovieModel, getAllMovies }
+const addASingleMovie = async (objectData) => {
+  try{
+    const {title, director, releaseYear, genre, rating, duration, cast } = objectData
+    if(!title || !director || !releaseYear || !genre || !rating || !duration || !cast){
+    throw new Error('All the fields must be present')
+    }
+    const result = await MovieModel.create(objectData)
+    return result
+
+  }catch(err){
+    throw new Error(err.message)
+  }
+}
+
+
+const updateAMovie = async (movieId, movieData) => {
+  try{
+    if(!ObjectId.isValid(movieId)){
+      throw new Error('Your movie ID is incorrect')
+    }
+    // const {title, director, releaseYear, genre, rating, duration, cast} = movieData
+    const result = await MovieModel.findOneAndUpdate({ _id: new ObjectId(movieId) }, { $set: movieData }, {new: true})
+    if(result.matchedCount === 0){
+      return {message: 'No movie was found with this ID'}
+    }
+    if(result.modifiedCount ===0){
+      return {message: 'No changes made, the contact data is already up to date.'}
+    }
+    return {
+      message: 'Contact updated successfully',
+      modifiedCount: result.modifiedCount
+    }
+  }catch(err){
+    console.error(err.message)
+    return {err: err.message}
+  }
+}
+
+const deleteAMovie = async (movieId) => {
+  try{
+    if(!ObjectId.isValid(movieId)){
+      throw new Error('No contact found with the given ID')
+    }
+    const result = await MovieModel.findOneAndDelete({_id: new ObjectId(movieId)})
+    if(!result) {
+      throw new Error('No contact was deleted.')
+    }
+    return {
+      message: 'Contact deleted succesfully',
+      deletedMovie: result
+    }
+  }catch(err) {
+    console.error(err.message)
+    return {error: err.message}
+  }
+}
+
+module.exports = { MovieModel, getAllMovies, addASingleMovie, updateAMovie, deleteAMovie}
+
+
+
+
+
